a89afc289cb00b57fcb0b357959cc5d36c39462b
Marcelo Toledo <marcelotoledo5000@gmail.com>
2025-07-07 21:09:44 -0300

    Update queens_puzzle.jsx

--- queens_puzzle.jsx
+++ queens_puzzle.jsx
@@ -73,11 +73,18 @@ export default function App() {
   const [userId, setUserId] = React.useState(null);
   const [isAuthReady, setIsAuthReady] = React.useState(false);
 
-  const appId = typeof __app_id !== 'undefined' ? __app_id : 'puzzle-tracker-app';
+  const appId = process.env.REACT_APP_FIREBASE_PROJECT_ID || 'puzzle-tracker-app';
 
   React.useEffect(() => {
     try {
-        const firebaseConfig = typeof __firebase_config !== 'undefined' ? JSON.parse(__firebase_config) : {};
+        const firebaseConfig = {
+          apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+          authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+          projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+          storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.REACT_APP_FIREBASE_APP_ID
+        };
         const app = initializeApp(firebaseConfig);
         const firestoreDb = getFirestore(app);
         const firebaseAuth = getAuth(app);
