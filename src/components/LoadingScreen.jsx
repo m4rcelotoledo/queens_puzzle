036c96dba8d751151e1463bf13b7ace8d37e56db
Marcelo Toledo <marcelotoledo5000@gmail.com>
2026-03-28 12:26:17 -0300

    Update LoadingScreen Component

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const LoadingScreen = ({ message }) => (
-  <div className="min-h-screen bg-gray-100 flex items-center justify-center text-gray-600 font-semibold text-lg">
-    {message}...
+const LoadingScreen = ({ message, footer }) => (
+  <div className="min-h-screen bg-gray-100 dark:bg-gray-900 flex flex-col">
+    <div className="flex-1 flex items-center justify-center text-gray-600 dark:text-gray-400 font-semibold text-lg">
+      {message}...
+    </div>
+    {footer}
   </div>
 );
 
