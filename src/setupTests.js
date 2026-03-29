49d66794b1aafe96f85b69e228c820b009ada446
Marcelo Toledo <marcelotoledo5000@gmail.com>
2026-03-29 00:38:24 -0300

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,6 +3,8 @@ import '@testing-library/jest-dom';
 // Firebase mocks
 jest.mock('firebase/app', () => ({
   initializeApp: jest.fn(() => ({})),
+  getApps: jest.fn(() => []),
+  getApp: jest.fn(() => ({})),
 }));
 
 jest.mock('firebase/analytics', () => ({
