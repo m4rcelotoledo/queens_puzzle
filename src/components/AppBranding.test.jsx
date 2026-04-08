fd56d22ad977fdebfa4748878b2f38bb3ff62f29
Marcelo Toledo <marcelotoledo5000@gmail.com>
2026-04-08 12:51:16 -0300

    AppBranding.test.jsx

--- src/components/AppBranding.test.jsx
+++ src/components/AppBranding.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import AppBranding from '../../../src/components/AppBranding';
+import AppBranding from './AppBranding';
 
 describe('AppBranding', () => {
   it('renders title and tagline', () => {
